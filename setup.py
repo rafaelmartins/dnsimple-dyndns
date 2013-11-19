cf934815c51a199cd1e980053d1a3b5f90a05789
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2013-11-19 00:35:52 -0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(current_dir, 'README.md')) as fp:
+    long_description = fp.read()
+
+setup(
+    name='dnsimple-dyndns',
+    version='0.1',
+    license='BSD',
+    description='Dynamic DNS implementation, that relies on DNSimple.com.',
+    long_description=long_description,
+    author='Rafael Goncalves Martins',
+    author_email='rafael@rafaelmartins.eng.br',
+    url='https://github.com/rafaelmartins/dnsimple-dyndns',
+    packages=find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+    ],
+    entry_points={'console_scripts':
+                  ['dnsimple-dyndns = dnsimple_dyndns:main']},
+)
