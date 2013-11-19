118a8ad753332d050957d74b573e5e44e57a433d
Jayson Reis <santosdosreis@gmail.com>
2013-11-19 15:06:40 -0200

    added more tests

--- tests/test_dnsimple.py
+++ tests/test_dnsimple.py
@@ -51,3 +51,87 @@ def test_get_record_error(patched_session, dnsimple):
 
     with pytest.raises(RuntimeError):
         dnsimple._get_record('ok')
+
+
+def test_create_record(patched_session, dnsimple):
+    response = mock.Mock(ok=True)
+    response.json.return_value = {'record': {'id': 10, 'content': '127.0.0.1'}}
+
+    patched_session().post.return_value = response
+    assert {'id': 10, 'content': '127.0.0.1'} == dnsimple._create_record('ok', '127.0.0.1', 60)
+    patched_session().post.assert_called_once_with(dnsimple._baseurl,
+                                                   headers={'Content-Type': 'application/json'},
+                                                   data='{"record": {"content": "127.0.0.1", "record_type": "A", "name": "ok", "ttl": 60}}')
+
+
+def test_create_record_invalid_http_code(patched_session, dnsimple):
+    response = mock.Mock(ok=False)
+    response.json.return_value = 'anything'
+
+    patched_session().post.return_value = response
+    with pytest.raises(RuntimeError):
+        dnsimple._create_record('ok', '127.0.0.1', 60)
+
+
+def test_create_record_invalid_json_format(patched_session, dnsimple):
+    response = mock.Mock(ok=True)
+    response.json.return_value = {}
+
+    patched_session().post.return_value = response
+    with pytest.raises(RuntimeError):
+        dnsimple._create_record('ok', '127.0.0.1', 60)
+
+
+def test_update_record(patched_session, dnsimple):
+    response = mock.Mock(ok=True)
+    response.json.return_value = {'record': {'id': 10, 'content': '127.0.0.1'}}
+
+    patched_session().put.return_value = response
+    assert {'id': 10, 'content': '127.0.0.1'} == dnsimple._update_record(10, 'ok', '127.0.0.1', 60)
+    patched_session().put.assert_called_once_with('%s/10' % dnsimple._baseurl,
+                                                  headers={'Content-Type': 'application/json'},
+                                                  data='{"record": {"content": "127.0.0.1", "name": "ok", "ttl": 60}}')
+
+
+def test_update_record_invalid_http_code(patched_session, dnsimple):
+    response = mock.Mock(ok=False)
+    response.json.return_value = 'anything'
+
+    patched_session().put.return_value = response
+    with pytest.raises(RuntimeError):
+        dnsimple._update_record(10, 'ok', '127.0.0.1', 60)
+
+
+def test_update_record_invalid_json_format(patched_session, dnsimple):
+    response = mock.Mock(ok=True)
+    response.json.return_value = {}
+
+    patched_session().put.return_value = response
+    with pytest.raises(RuntimeError):
+        dnsimple._update_record(10, 'ok', '127.0.0.1', 60)
+
+
+def test_main_update_record_with_existing_record(patched_session, dnsimple):
+    get_record = mock.Mock(ok=True)
+    get_record.json.return_value = [{'record': {'id': 1}}]
+
+    update_record = mock.Mock(ok=True)
+    update_record.json.return_value = {'record': {'id': 10, 'content': '127.0.0.1'}}
+
+    patched_session().get.return_value = get_record
+    patched_session().put.return_value = update_record
+
+    assert {'id': 10, 'content': '127.0.0.1'} == dnsimple.update_record('ok', '127.0.0.1', 60)
+
+
+def test_main_update_record_with_non_existing_record(patched_session, dnsimple):
+    get_record = mock.Mock(ok=True)
+    get_record.json.return_value = []
+
+    create_record = mock.Mock(ok=True)
+    create_record.json.return_value = {'record': {'id': 10, 'content': '127.0.0.1'}}
+
+    patched_session().get.return_value = get_record
+    patched_session().post.return_value = create_record
+
+    assert {'id': 10, 'content': '127.0.0.1'} == dnsimple.update_record('ok', '127.0.0.1', 60)
